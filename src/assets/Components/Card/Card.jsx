752639d7cc671be7c5943d8f1668cbf5c5144484
Hossain-sakib <mskib567@gmail.com>
2023-09-16 22:14:30 +0600

    Updated the description of the cards

--- src/assets/Components/Card/Card.jsx
+++ src/assets/Components/Card/Card.jsx
@@ -4,7 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const Card = ({ card, handleSelect }) => {
     const { title, image, description, price, credit_hour } = card;
     return (
-        <div className="bg-white rounded-2xl shadow-xl px-4 py-4 space-y-4">
+        <div className=" bg-white rounded-2xl shadow-xl px-4 py-4 space-y-4">
             <div>
                 <img className="w-full" src={image} alt="" />
             </div>
@@ -12,7 +12,7 @@ const Card = ({ card, handleSelect }) => {
                 <h2 className="text-xl font-medium">{title}</h2>
                 <p className="text-sm text-slate-500 font-light ">{description}</p>
             </div>
-            <div className="flex justify-between lg:gap-4">
+            <div className=" flex justify-between lg:gap-4">
                 <div className="flex flex-1 items-center lg:gap-2">
                     <img src="https://i.ibb.co/crS5nby/dollar-sign-1.png" alt="" />
                     <p className="text-xs text-slate-500 font-light ">Price: {price}</p>
@@ -22,7 +22,7 @@ const Card = ({ card, handleSelect }) => {
                     <p className="text-xs text-slate-500 font-light ">Credit: {credit_hour}hr</p>
                 </div>
             </div>
-            <div className="flex justify-center bg-blue-500 rounded-lg">
+            <div className="flex justify-center bg-blue-500 rounded-lg ">
                 <button onClick={() => handleSelect(card, price, credit_hour)} className="w-full text-lg font-bold text-white py-3 ">Select</button>
                 <ToastContainer />
             </div>
